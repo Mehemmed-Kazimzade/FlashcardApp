6c228cb8b92030b017c01b7eba54e9060f0976e7
Mehemmed Kazimzade <mehemmedkazimzade54@gmail.com>
2025-08-16 02:25:52 +0400

    s

--- src/components/Flashcard.jsx
+++ src/components/Flashcard.jsx
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion';
-import '../css/FlipStyles.css';
+import '../css/flipStyles.css';
 import CardBack from '../flipAnimationComponents/CardBack';
 import CardFront from '../flipAnimationComponents/CardFront';
 
